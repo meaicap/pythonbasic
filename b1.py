5694c94ff144c697bb5c492f378c2480b1967a45
meaicap <70737722+meaicap@users.noreply.github.com>
2023-12-16 08:26:03 +0700

    Update b1.py

--- b1.py
+++ b1.py
@@ -1,1 +1,6 @@
-
+Ghi chú một dòng #
+Ghi chú nhiều dòng """
+2. Bố cục mã trong python 
+Không cần giấy ; cuối câu """
+Không có cặp {} được thay thế bằng TAB vào phía bên trong       
+Shift tab để thụt lề trái.
